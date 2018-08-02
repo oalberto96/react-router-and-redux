e0db70c5385631886680eb5882c4223ba5bfe97a
oalberto96 <ochoapublic@gmail.com>
2018-08-02 14:17:10 -0700

    Finished tutorial 01 - Personal website

---
+++ module01/tut/src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <h2> My name is Alberto Ochoa and I am a React Developer. </h2>;
+};
+
+export default About;
---
+++ module01/tut/src/components/Resume.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div>
+      <h1> Alberto Ochoa </h1>
+      <h2> Education </h2>
+      <h3> Universidad Autonoma de Baja California </h3>
+      <ul>
+        <li> Bachelors of Computer Engineering </li>
+        <li> Class of 2014 </li>
+      </ul>
+      <h2> Work History </h2>
+      <h3> Web developer </h3>
+      <ul>
+        <li> Created a website for Geomaya.</li>
+      </ul>{" "}
+      <h2> Skills </h2>
+      <ul>
+        <li> React.js </li>
+        <li> React Router </li>
+        <li> Node.js </li>
+        <li> Python </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Resume;
